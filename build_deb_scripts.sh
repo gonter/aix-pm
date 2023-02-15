dba05d9aefa8c517384b946c01cf8254aa8970fc
Gerhard Gonter <ggonter@gmail.com>
2023-02-15 05:15:32 +0100

    added build for scripts and renamend build for lib

---
+++ build_deb_scripts.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+epoch=0
+version=`awk '/Version:/ {print $2}' scripts/debian/control`
+pkgdir=tmp/stage/gg-aix-pm-tools/$epoch/$version
+bindir=$pkgdir/data/usr/bin
+
+mkdir -p $pkgdir/control
+
+cp scripts/debian/control $pkgdir/control
+cp debian/debian-binary $pkgdir
+
+mkdir -p $bindir
+cp scripts/mkdeb.pl $bindir/
+cp modules/util/csv.pl $bindir/
+
+scripts/mkdeb.pl $pkgdir
