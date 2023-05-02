e0858e9e8108aa75f755cff6bd93a444f553af7b
Gerhard Gonter <ggonter@gmail.com>
2023-05-02 17:13:06 +0200

    updated version number for lib and script packages to the same value

--- build_deb_scripts.sh
+++ build_deb_scripts.sh
@@ -2,7 +2,7 @@
 
 epoch=0
 version=`awk '/Version:/ {print $2}' scripts/debian/control`
-pkgname=`awk '/Package:/ {print $2}' debian/control`
+pkgname=`awk '/Package:/ {print $2}' scripts/debian/control`
 pkgdir=tmp/stage/$pkgname/$epoch/$version
 
 bindir=$pkgdir/data/usr/bin
