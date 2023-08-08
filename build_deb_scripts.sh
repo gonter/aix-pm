7724d0e15b02ff9dec1af1ec9ba7f2a981e85594
Gerhard Gonter <ggonter@gmail.com>
2023-08-08 19:53:34 +0200

    updated build script and version number

--- build_deb_scripts.sh
+++ build_deb_scripts.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 epoch=0
-version=`awk '/Version:/ {print $2}' scripts/debian/control`
-pkgname=`awk '/Package:/ {print $2}' scripts/debian/control`
+version=`awk '/^Version:/ {print $2}' scripts/debian/control`
+pkgname=`awk '/^Package:/ {print $2}' scripts/debian/control`
 pkgdir=tmp/stage/$pkgname/$epoch/$version
 
 bindir=$pkgdir/data/usr/bin
