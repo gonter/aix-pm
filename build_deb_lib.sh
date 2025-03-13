b6b0951e28dff2a690cb69d052d882c0b2c13517
Gerhard Gonter <ggonter@gmail.com>
2025-03-13 19:14:59 +0100

    restructred debian package metadata

--- build_deb_lib.sh
+++ build_deb_lib.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 epoch=0
-version=`awk '/Version:/ {print $2}' debian/control`
-pkgname=`awk '/Package:/ {print $2}' debian/control`
+version=`awk '/Version:/ {print $2}' debian/lib/control`
+pkgname=`awk '/Package:/ {print $2}' debian/lib/control`
 pkgdir=tmp/stage/$pkgname/$epoch/$version
 
 scripts/pmlnk.pm --copy --target $pkgdir/data/usr/share/perl5 .
@@ -10,9 +10,9 @@ scripts/pmlnk.pm --copy --target $pkgdir/data/usr/share/perl5 .
 chmod +x $pkgdir/data/usr/share/perl5/Net/fanout.pm
 
 mkdir $pkgdir/control
-cp debian/control $pkgdir/control
+cp debian/lib/control $pkgdir/control
 echo "2.0" >$pkgdir/debian-binary
 
 echo scripts/mkdeb.pl $pkgdir $*
-scripts/mkdeb.pl $pkgdir $*
+scripts/mkdeb.pl --gz $pkgdir $*
 
--- build_deb_scripts.sh
+++ build_deb_scripts.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
 epoch=0
-version=`awk '/^Version:/ {print $2}' scripts/debian/control`
-pkgname=`awk '/^Package:/ {print $2}' scripts/debian/control`
+version=`awk '/^Version:/ {print $2}' debian/scripts/control`
+pkgname=`awk '/^Package:/ {print $2}' debian/scripts/control`
 pkgdir=tmp/stage/$pkgname/$epoch/$version
 
 bindir=$pkgdir/data/usr/bin
 mkdir -p $pkgdir/control
 
-cp scripts/debian/control $pkgdir/control
+cp debian/scripts/control $pkgdir/control
 echo "2.0" >$pkgdir/debian-binary
 
 mkdir -p $bindir
 cp scripts/mkdeb.pl $bindir/
+cp hacks/linux/lvm/chfs.pl $bindir/
+cp hacks/linux/lvm/cfgmgr.pl $bindir/
 cp modules/util/csv.pl $bindir/
 ln -s ../share/perl5/Net/fanout.pm $bindir/fanout
+ln -s ../share/perl5/pmlnk.pm $bindri/pmlnk
 
 echo scripts/mkdeb.pl $pkgdir $*
-scripts/mkdeb.pl $pkgdir $*
+scripts/mkdeb.pl --gz $pkgdir $*
 
